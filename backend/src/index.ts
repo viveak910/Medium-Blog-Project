38ed0dc9e79028521d03c05b1e22a6ce0c1bec20
viveak910 <1602-23-737-191@vce.ac.in>
2024-08-15 19:23:46 +0530

    added common to deploy npm package online and use,optimized whole code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,16 @@
 import { Hono } from 'hono'
-import userRouter from './routes/user'
-import blogRouter from './routes/blog'
-import { PrismaClient } from '@prisma/client/edge';
-import { withAccelerate } from '@prisma/extension-accelerate';
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
+import { cors } from 'hono/cors'
 
-export const app = new Hono<{
+const app = new Hono<{
   Bindings: {
-      DATABASE_URL: string;
-      JWT_SECRET: string;
+    DATABASE_URL: string;
+    JWT_SECRET: string;
   }
 }>();
-
-app.route('/api/v1/user', userRouter)
-app.route('/api/v1/book', blogRouter)
+app.use('/*', cors())
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 export default app
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
-import { sign } from "hono/jwt";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
+import { signupInput, signinInput } from "@viveak910/common";
 
 export const userRouter = new Hono<{
     Bindings: {
@@ -11,47 +12,74 @@ export const userRouter = new Hono<{
 }>();
 
 userRouter.post('/signup', async (c) => {
+    const body = await c.req.json();
+    const { success } = signupInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            message: "Inputs not correct"
+        })
+    }
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
+    }).$extends(withAccelerate())
   
-    const body = await c.req.json();
-  
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
-      },
-    });
-  
-    const token = await sign({ id: user.id }, c.env.JWT_SECRET)
-  
-    return c.json({
-      jwt: token
-    })
-})
-  
-userRouter.post('/signin', async (c) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env?.DATABASE_URL	,
-    }).$extends(withAccelerate());
-
-    const body = await c.req.json();
-    const user = await prisma.user.findUnique({
-        where: {
-            email: body.email,
-            password: body.password
+    try {
+      const user = await prisma.user.create({
+        data: {
+          username: body.username,
+          password: body.password,
+          name: body.name
         }
-    });
-
-    if (!user) {
-        c.status(403);
-        return c.json({ error: "user not found" });
+      })
+      const jwt = await sign({
+        id: user.id
+      }, c.env.JWT_SECRET);
+  
+      return c.text(jwt)
+    } catch(e) {
+      console.log(e);
+      c.status(411);
+      return c.text('Invalid')
+    }
+  })
+  
+  
+  userRouter.post('/signin', async (c) => {
+    const body = await c.req.json();
+    const { success } = signinInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            message: "Inputs not correct"
+        })
     }
 
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-    
-    return c.json({ jwt });
-})
-
-export default userRouter;
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+  
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          username: body.username,
+          password: body.password,
+        }
+      })
+      if (!user) {
+        c.status(403);
+        return c.json({
+          message: "Incorrect creds"
+        })
+      }
+      const jwt = await sign({
+        id: user.id
+      }, c.env.JWT_SECRET);
+  
+      return c.text(jwt)
+    } catch(e) {
+      console.log(e);
+      c.status(411);
+      return c.text('Invalid')
+    }
+  })
