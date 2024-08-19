aa7e8a72af3151b94d7c1375c349bbc989c96e24
viveak910 <1602-23-737-191@vce.ac.in>
2024-08-19 14:44:08 +0530

    Quote file with ui part added

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,13 @@
+
+import { Quote } from "../components/Quote"
+
+
+export const Signup = () => {
+    return <div className="grid grid-cols-2">
+        <div>hello</div>
+        <div className="invisible md:visible">
+            <Quote/>
+        </div>
+        
+    </div>
+}
